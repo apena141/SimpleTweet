cdbd3d7512daf6c7405547960068a678c2cbe4b7
apena141 <apena141@fiu.edu>
2021-02-06 14:11:52 -0500

    Initial commit

---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/TimelineActivity.java
@@ -0,0 +1,67 @@
+package com.codepath.apps.restclienttemplate;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.codepath.apps.restclienttemplate.models.Tweet;
+import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import okhttp3.Headers;
+
+public class TimelineActivity extends AppCompatActivity {
+
+    RecyclerView rvTweets;
+    TweetsAdapter adapter;
+    public static final String TAG = "TimeLineActivity";
+    TwitterClient client;
+    List<Tweet> tweets;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+        client = TwitterApp.getRestClient(this);
+
+        // Find the Recycler View
+        rvTweets = findViewById(R.id.rvTweets);
+        // Initialize the list of tweets and adapter
+        tweets = new ArrayList<>();
+        adapter = new TweetsAdapter(this, tweets);
+        // Recycler view setup: layout manager and adapter
+        rvTweets.setLayoutManager(new LinearLayoutManager(this));
+        rvTweets.setAdapter(adapter);
+
+        populateHomeTimeLine();
+    }
+
+    private void populateHomeTimeLine() {
+        client.getHomeTimeline(new JsonHttpResponseHandler() {
+            @Override
+            public void onSuccess(int statusCode, Headers headers, JSON json) {
+                Log.d(TAG, "onSuccess! " + json.toString());
+                JSONArray jsonArray = json.jsonArray;
+                try{
+                    tweets.addAll(Tweet.fromJsonArray(jsonArray));
+                    adapter.notifyDataSetChanged();
+                }catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void onFailure(int statusCode, Headers headers, String response, Throwable throwable) {
+                Log.e(TAG, "onFailure " + response, throwable);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/codepath/apps/restclienttemplate/models/Tweet.java
@@ -0,0 +1,40 @@
+package com.codepath.apps.restclienttemplate.models;
+
+import com.codepath.apps.restclienttemplate.TimeFormatter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Tweet {
+    public String body;
+    public String createdAt;
+    public User user;
+    public String timeStamp;
+
+    // Given a JSONObject we want to convert it to a tweet, each object is a tweet
+    public static Tweet fromJson(JSONObject jsonObject) throws JSONException {
+        Tweet tweet = new Tweet();
+        tweet.body = jsonObject.getString("text");
+        tweet.createdAt = jsonObject.getString("created_at");
+        tweet.user = User.fromJson(jsonObject.getJSONObject("user"));
+        return tweet;
+    }
+
+    // Create a List of tweets from the jsonArray and return the list of tweets
+    public static List<Tweet> fromJsonArray(JSONArray jsonArray) throws JSONException {
+        List<Tweet> tweets = new ArrayList<>();
+        for(int i = 0; i < jsonArray.length(); i++){
+            tweets.add(fromJson(jsonArray.getJSONObject(i)));
+        }
+        return tweets;
+    }
+
+    public String getFormattedTimeStamp(){
+        return TimeFormatter.getTimeDifference(this.createdAt);
+    }
+
+}
